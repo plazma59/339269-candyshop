14ba1385e0934201f695b8826c2787ee116c1fe3
Daria <darya_chinnova@mail.ru>
2018-10-10 03:53:42 +0300

    6

---
+++ js/backend.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/candyshop/data';
+  window.backend = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+})();
+
+(function () {
+  var URL = 'https://js.dump.academy/candyshop';
+
+  window.upload = function (data, onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+})();
